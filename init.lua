d26534357c5085dce604cf5446aa7a105dff2e4b
Euclideus <143367005+Euclidae@users.noreply.github.com>
2024-12-14 21:56:53 +0300

    Updated init lua again

--- init.lua
+++ init.lua
@@ -1,5 +1,6 @@
 vim.g.base46_cache = vim.fn.stdpath "data" .. "/nvchad/base46/"
 vim.g.mapleader = " "
+vim.opt.shada = "'1000,<50,s10,h"
 
 -- bootstrap lazy and all plugins
 local lazypath = vim.fn.stdpath "data" .. "/lazy/lazy.nvim"
